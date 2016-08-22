111889c6bee491f902c0685a060a22124383aecf
dnipromv <dnipromv@gmail.com>
2016-08-22 20:06:44 +0200

    add row hints calculation

--- main.lua
+++ main.lua
@@ -30,3 +30,38 @@ for r=1,matrix.size do
 	print(stringLine)
 end
 
+-- array of nonogram row hints
+local rHints = {};
+
+
+for r=1,matrix.size do
+	local rowHints = {}; -- row hints only for current row
+	-- loop through columns in search of shaded segments
+
+	local c=1;
+	-- WHILE loop allows to change iteration value inside of the loop (and FOR doesn't)
+	while c<=matrix.size do
+		local segment = matrix[r][c];
+		if segment==1 then
+			-- if segment is shaded - add new row hint with value 1
+			rowHints[#rowHints+1] = 1;
+
+			-- check neibhor segments
+			c = c + 1;
+			local nextSegment = matrix[r][c];
+			while nextSegment==1 do
+				-- if next segment is shaded - increase row hint value
+				rowHints[#rowHints] = rowHints[#rowHints] + 1;
+
+				-- set next segment
+				c = c + 1;
+				nextSegment = matrix[r][c];
+			end
+		else
+			c = c+1;
+		end
+
+	end
+	rHints[r] = rowHints;
+end
+
